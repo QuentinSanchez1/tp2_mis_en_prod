8a35918fa4900843f782ab7ae19c068f71ace8a7
QuentinSanchez1 <quentin-sanchez@outlook.fr>
2024-10-30 14:51:22 +0100

    modif des fichiers

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-def addition(a,b):
+def addition(a, b):
     return a + b
-def soustraction(c,d):
+def soustraction(c, d):
     return c - d
 
-if name == "main":
-    result = addition(5,7)
-    result2 = soustraction(5,7)
+if __name__ == "__main__":
+    result = addition(5, 7)
+    result2 = soustraction(5, 7)
     print(f"Le résultat est le suivant : {result}")
     print(f"Le résultat est le suivant : {result2}")
--- test_app.py
+++ test_app.py
@@ -2,18 +2,18 @@ import unittest
 from app import addition
 
 class TestAddition(unittest.TestCase):
-    def testaddition(self):
-        self.assertEqual(addition(2, 3),5)
-        self.assertEqual(addition(-1, 1),0)
-        self.assertEqual(addition(0, 0),0)
+    def test_addition(self):
+        self.assertEqual(addition(2, 3), 5)
+        self.assertEqual(addition(-1, 1), 0)
+        self.assertEqual(addition(0, 0), 0)
 
 from app import soustraction
 
 class TestSoustraction(unittest.TestCase):
-    def testaddition(self):
-        self.assertEqual(soustraction(2, 3),-1)
-        self.assertEqual(soustraction(-1, 1),-2)
-        self.assertEqual(soustraction(0, 0),0)
+    def test_addition(self):
+        self.assertEqual(soustraction(2, 3), -1)
+        self.assertEqual(soustraction(-1, 1), -2)
+        self.assertEqual(soustraction(0, 0), 0)
 
-if name == '__main':
+if __name__ == '__main__':
     unittest.main()
