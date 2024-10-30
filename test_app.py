207c7a24d5ca6fc994eee4ebc8a8a33e74230e36
QuentinSanchez1 <quentin-sanchez@outlook.fr>
2024-10-30 14:34:46 +0100

    Ajout de la fonction soustraction et tests associés

--- test_app.py
+++ test_app.py
@@ -3,17 +3,17 @@ from app import addition
 
 class TestAddition(unittest.TestCase):
     def testaddition(self):
-        self.assertEqual(addition(2, 3), 5)
-        self.assertEqual(addition(-1, 1), 0)
-        self.assertEqual(addition(0, 0), 0)
+        self.assertEqual(addition(2, 3),5)
+        self.assertEqual(addition(-1, 1),0)
+        self.assertEqual(addition(0, 0),0)
 
 from app import soustraction
 
 class TestSoustraction(unittest.TestCase):
     def testaddition(self):
-        self.assertEqual(soustraction(2, 3), -1)
-        self.assertEqual(soustraction(-1, 1), -2)
-        self.assertEqual(soustraction(0, 0), 0)
+        self.assertEqual(soustraction(2, 3),-1)
+        self.assertEqual(soustraction(-1, 1),-2)
+        self.assertEqual(soustraction(0, 0),0)
 
 if name == '__main':
     unittest.main()
